72470dc45bd939d3da3a96454291770b0fae2c69
Yannick Djopgang <nzeneyannick@yahoo.fr>
2019-09-16 18:02:06 +0200

    code review 1 : modification du type Timesteam pour LocalDate et creation du mapper et dto : non achevé

--- com/excilys/entities/Computer.java
+++ com/excilys/entities/Computer.java
@@ -1,75 +1,74 @@
 package com.excilys.entities;
 
-import java.sql.Timestamp;
-import java.util.*;
+import java.time.LocalDate;
 
-//@SuppressWarnings("unused")
+// @SuppressWarnings("unused")
 public class Computer {
 
-	private int id;
-	private String name;
-	private Timestamp introduced;
-	private Timestamp discontinued;
-	private Company compagnie;
+  private int id;
+  private String name;
+  private LocalDate introduced;
+  private LocalDate discontinued;
+  private Company compagnie;
 
-	public Computer() {
+  public Computer() {
 
-	}
+  }
 
-	public Computer(int id, String name, Timestamp introduced, Timestamp discontinued, Company compagnie) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.introduced = introduced;
-		this.discontinued = discontinued;
-		this.compagnie = compagnie;
-	}
+  public Computer(int id, String name, LocalDate introduced, LocalDate discontinued,
+      Company compagnie) {
+    super();
+    this.id = id;
+    this.name = name;
+    this.introduced = introduced;
+    this.discontinued = discontinued;
+    this.compagnie = compagnie;
+  }
 
-	public int getId() {
-		return id;
-	}
+  public int getId() {
+    return id;
+  }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+  public void setId(int id) {
+    this.id = id;
+  }
 
-	public String getName() {
-		return name;
-	}
+  public String getName() {
+    return name;
+  }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+  public void setName(String name) {
+    this.name = name;
+  }
 
-	public Timestamp getIntroduced() {
-		return introduced;
-	}
+  public LocalDate getIntroduced() {
+    return introduced;
+  }
 
-	public void setIntroduced(Timestamp introduced) {
-		this.introduced = introduced;
-	}
+  public void setIntroduced(LocalDate introduced) {
+    this.introduced = introduced;
+  }
 
-	public Timestamp getDiscontinued() {
-		return discontinued;
-	}
+  public LocalDate getDiscontinued() {
+    return discontinued;
+  }
 
-	public void setDiscontinued(Timestamp discontinued) {
-		this.discontinued = discontinued;
-	}
+  public void setDiscontinued(LocalDate discontinued) {
+    this.discontinued = discontinued;
+  }
 
-	public Company getCompagnie() {
-		return compagnie;
-	}
+  public Company getCompagnie() {
+    return compagnie;
+  }
 
-	public void setCompagnie(Company compagnie) {
-		this.compagnie = compagnie;
-	}
+  public void setCompagnie(Company compagnie) {
+    this.compagnie = compagnie;
+  }
 
-	@Override
-	public String toString() {
-		return "Computer [id=" + id + ", name=" + name + ", introduced=" + introduced + ", discontinued=" + discontinued
-				+ ", compagnieId=" + compagnie.getId() + "]";
-	}
+  @Override
+  public String toString() {
+    return "Computer [id=" + id + ", name=" + name + ", introduced=" + introduced
+        + ", discontinued=" + discontinued + ", compagnieId=" + compagnie.getId() + "]";
+  }
 
-	
 }
